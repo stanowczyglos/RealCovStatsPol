be975a2d8aa09d47c7df4ba679d77c72c42486ae
stanowczyglos <96392768+stanowczyglos@users.noreply.github.com>
2021-12-22 00:20:30 +0100

    1. Modify 3months.csv for proper dates; 2. Remove *.ods as they are no longer needed for updates; 3. Create updateData.sh script for automatic upload and parse of required data; 4. Update OWID data; 5. Create OWID tmp file countriesExtract.csv with specific countries; 6. Update Germany data; 7. Create python script for parsing data from Polish gov; 8. Create tmp file poland.csv for tracking data;

---
+++ OfficialDataSrc/Poland/MSZExtractor.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+
+import csv
+import os, sys
+
+if __name__ == '__main__':
+    allDeathIdx = 0
+    covDeathIdx = 0
+    dateIdx = 0
+    outCsv = "Data;Zgony;Zgony COV" + os.linesep
+    files = os.listdir(".")
+    files.sort()
+    for filename in files:
+        if True == filename.endswith('eksport.csv'):
+            print(filename)
+            csvRow = filename.split('_')[0][:8]
+            with open(filename, newline='', encoding='iso-8859-1') as csvFile:
+                reader = csv.reader(csvFile, delimiter=';')
+                row = next(reader)
+                iter = 0
+                for col in row:
+                    if col == "zgony":
+                        allDeathIdx = iter
+                    elif -1 != col.find("bez_chorob_wspolistniejacych"):
+                        covDeathIdx = iter
+                    iter += 1
+                row = next(reader)
+                csvRow += ';' + str(row[allDeathIdx]) + ';' + str(row[covDeathIdx]) + os.linesep
+                outCsv+= csvRow
+    with open("poland.csv", newline='', encoding='iso-8859-1', mode='w') as csvFile:
+        csvFile.write(outCsv)
+    
+    
---
+++ OfficialDataSrc/updateData.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#get RKI report and convert it to csv
+cd Germany_RKI
+wget https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Fallzahlen_Gesamtuebersicht.xlsx?__blob=publicationFile -O Fallzahlen_Gesamtuebersicht.xlsx
+libreoffice --headless --convert-to "csv:Text - txt - csv (StarCalc):59,34,76,1,1/1" Fallzahlen_Gesamtuebersicht.xlsx
+rm Fallzahlen_Gesamtuebersicht.xlsx
+cd ..
+
+#get OWID data
+cd OWID
+wget https://covid.ourworldindata.org/data/owid-covid-data.csv -O owid-covid-data.csv
+./owidExtractor.py
+cd ..
+
+#get Polish data
+cd Poland
+wget https://arcgis.com/sharing/rest/content/items/a8c562ead9c54e13a135b02e0d875ffb/data -O arch.zip
+unzip -o arch.zip
+rm arch.zip
+./MSZExtractor.py
+cd ..
