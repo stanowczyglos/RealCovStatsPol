be7ed8d3c75a4572596d932402c486dc977401c1
stanowczyglos <96392768+stanowczyglos@users.noreply.github.com>
2021-12-29 14:53:06 +0100

    1. Update data; 2. Create one script - deathAnalyser.py, for creating 3months.csv and sinceBeginning.csv files automatically; 3. The rest of the python scripts will be merged to this one soon;

--- Scripts/deathAnalyser.py
+++ Scripts/deathAnalyser.py
@@ -8,17 +8,122 @@ import pandas as pd
 from zipfile import ZipFile
 from urllib.request import urlretrieve
 import urllib
+import configparser
+import itertools
+import logging
 
 #data sources to be downloaded
 polUrl = 'https://arcgis.com/sharing/rest/content/items/a8c562ead9c54e13a135b02e0d875ffb/data'
 deUrl = 'https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Fallzahlen_Gesamtuebersicht.xlsx?__blob=publicationFile'
 owidUrl = 'https://covid.ourworldindata.org/data/owid-covid-data.csv'
 
-#config
-#modify below line to pick start date
-startDate = datetime.today().replace(hour=0, minute=0, second=0, microsecond=0) - timedelta(90)
+filePathPol = os.path.join("..", "OfficialDataSrc", "Poland", "DeathData", "arch.zip")
+filePathDe = os.path.join("..", "OfficialDataSrc", "Germany_RKI", "Fallzahlen_Gesamtuebersicht.xlsx")
+filePathDeCsv = os.path.join("..", "OfficialDataSrc", "Germany_RKI", "Fallzahlen_Gesamtuebersicht.csv")
+filePathOwid = os.path.join("..", "OfficialDataSrc", "OWID", "owid-covid-data.csv")
+
+filePath3Months = os.path.join("..", "calculatedData", "3months.csv")
+filePathSinceBeginning = os.path.join("..", "calculatedData", "sinceBeginning.csv")
+
+#Logger - empty stub for later usage as global logger
+log = None
+
+class DataHandler:    
+    #this function will be used for downloading and converting all data. For now it's hard to handle RKI xlsx.
+    def parsePercentCell(self, val):
+        if 0 != val:
+            return format(val*100, '.2f') + '%'
+        return ''
+    
+    def parseFloatToIntCell(self, val):
+        if 0 != val:
+            return int(val)
+        return ''
+    
+    def downloadData(self):
+        #download and prepare polish data
+        logging.info('Downloading polish data from: ' + polUrl)
+        req = urllib.request.Request(polUrl, headers={'User-Agent' : "Magic Browser"}) 
+        con = urllib.request.urlopen(req)
+        with open(filePathPol, 'wb') as file:
+            file.write(con.read())
+        with ZipFile(filePathPol, 'r') as zipObj:
+            zipObj.extractall(os.path.split(filePathPol)[0])
+        
+        #download and prepare german data
+        logging.info('Downloading german data from: ' + deUrl)
+        req = urllib.request.Request(deUrl, headers={'User-Agent' : "Magic Browser"}) 
+        con = urllib.request.urlopen(req)
+        with open(filePathDe, 'wb') as file:
+            file.write(con.read())
+        xlsx = pd.ExcelFile(filePathDe)
+        cols = ['Anzahl COVID-19-Fälle', 'Differenz Vortag Fälle', 'Todesfälle', 'Differenz Vortag Todesfälle', 'Fälle ohne Todesfälle']
+    
+        df = xlsx.parse(header=2)
+        for col in cols:
+            df[col] = df[col].fillna(value=0).apply(self.parseFloatToIntCell)
+        df['Fall-Verstorbenen-Anteil'] = df['Fall-Verstorbenen-Anteil'].fillna(value=0).apply(self.parsePercentCell)
+        df.to_csv(filePathDeCsv, encoding='utf-8', index=False)
+        
+        #download and prepare OWID data
+        logging.info('Downloading data for other countries from: ' + owidUrl)
+        req = urllib.request.Request(owidUrl, headers={'User-Agent' : "Magic Browser"}) 
+        con = urllib.request.urlopen(req)
+        with open(filePathOwid, 'wb') as file:
+            file.write(con.read())
+        
+        logging.info('Download complete')
+        return True
+    
+    def cleanupData(self):
+        logging.info('Remove: ' + filePathPol)
+        os.remove(filePathPol)
+        logging.info('Remove: ' + filePathOwid)
+        os.remove(filePathOwid)
+        logging.info('Remove: ' + filePathDe)
+        os.remove(filePathDe)
+        
+class GenericCsvDeathCreator:
+    def __init__(self, countries, columns):
+        dictInput = zip(countries, columns)
+        self.dict = dict(dictInput)
+        
+    def createCsv(self, startDate):
+        countryList = []
+        owidCountryList = []
+        for elem in self.dict.keys():
+            if elem == 'Poland':
+                pol = PolishDeathData()
+                logging.info('Parsing polish data')
+                pol.parser()
+                pol.createFullCsv()
+                countryList.append(pol.getDeathsFromDate(startDate))
+            elif elem == 'Germany':
+                de = GenericDeathData(0, -1, 4, filePathDeCsv, countryFilterList = [elem])
+                logging.info('Parsing german data')
+                de.parser(startDate)
+                countryList.extend(de.getDeaths())
+            else:
+                #Collect all OWID related countries to parse OWID file only once
+                owidCountryList.append(elem)
+        if owidCountryList != []:
+            owid = GenericDeathData(3, 2, 8, filePathOwid, countryFilterList = owidCountryList)
+            logging.info('Parsing ' + str(owidCountryList) + ' data')
+            owid.parser(startDate)
+            countryList.extend(owid.getDeaths())
+        
+        date_range = pd.date_range(start=startDate.strftime('%Y-%m-%d'), end=datetime.utcnow(), freq='d')
+        df = pd.DataFrame()
+        df['Data'] = date_range
+        for elem in countryList:
+            df[self.dict[elem.name]] = pd.Series(elem.data)
+            
+        with open(filePath3Months, 'w') as csvFile:
+            logging.info('Create ' + filePath3Months)
+            csvFile.write(df.to_csv(index = False))
+    
 class GenericCountryDeathData:
-    def __init__(self, name, dataList):
+    def __init__(self, name, dataList = []):
         self.name = name
         self.data = dataList
 
@@ -35,12 +140,11 @@ class PolishDeathData:
     def parser(self):
         allDeathIdx = 0
         covDeathIdx = 0
-        dataPath = '../OfficialDataSrc/Poland/DeathData/'
-        files = os.listdir(dataPath)
+        files = os.listdir(os.path.split(filePathPol)[0])
         files.sort()
         for filename in files:
             if True == filename.endswith('eksport.csv'):
-                with open(dataPath + filename, newline='', encoding='iso-8859-1') as csvFile:
+                with open(os.path.join(os.path.split(filePathPol)[0],filename), newline='', encoding='iso-8859-1') as csvFile:
                     reader = csv.reader(csvFile, delimiter=';')
                     row = next(reader)
                     iter = 0
@@ -54,24 +158,24 @@ class PolishDeathData:
                     self.deathList.append(self.DeathElem(filename.split('_')[0][:8], row[covDeathIdx], row[allDeathIdx]))
 
     def createFullCsv(self):
-        #display factor
-        factor = 1
         #initial value taken from official Gov data for 23.11.2020
-        allDeaths = 13774 * factor
+        allDeaths = 13774
         #below factor was calculated according to 2021 data. Deaths ratio between COV alone reason and COV with coexisting illnesses is 0.24
         covFactor = 0.24
         covDeaths = round(allDeaths * covFactor)
         
         outCsv = 'Data,Zgony COV+współistniejące,Zgony sam COV,Zgony COV+współistniejące narastająco,Zgony sam COV narastająco' + os.linesep
         for elem in self.deathList:
-            allDeaths += int(elem.deathCovMore) * factor
-            covDeaths += int(elem.deathCov) * factor
+            allDeaths += int(elem.deathCovMore)
+            covDeaths += int(elem.deathCov)
             outCsv += str(elem.date).split(' ')[0] + ',' + str(elem.deathCovMore) + ',' + str(elem.deathCov) + ',' + str(allDeaths) + ',' + str(covDeaths) + os.linesep
-        with open('../calculatedData/sinceBeginning.csv', newline='', mode='w') as csvFile:
+        with open(filePathSinceBeginning, newline='', mode='w') as csvFile:
+            logging.info('Create: ' + filePathSinceBeginning)
             csvFile.write(outCsv)
     
     def getDeathsFromDate(self, date):
-        return GenericCountryDeathData("Poland", [death.deathCov for death in self.deathList if death.date >= date])
+        list = [death.deathCov for death in self.deathList if death.date >= date]
+        return GenericCountryDeathData("Poland", list)
 
 class GenericDeathData:
     def __init__(self, dateCol, countryCol, deathCol, filePath, dateFormat = '%Y-%m-%d', countryFilterList = []):
@@ -83,121 +187,51 @@ class GenericDeathData:
         self.countryFilterList = countryFilterList
         self.countryData = []
     
-    def parser(self):
+    def parser(self, startDate):
         countryDataItem = []
         currentCountry = ''
         foundItem = False
         with open(self.filePath, newline='', encoding='iso-8859-1') as csvFile:
             reader = csv.reader(csvFile, delimiter=',')
+            #dummy read to skip header
+            next(reader)
             for row in reader:
+                #handler only for OWID data
+                if -1 != self.countryCol and currentCountry != '' and currentCountry != row[self.countryCol]:
+                    if foundItem == True:
+                        foundItem = False
+                        self.countryData.append(GenericCountryDeathData(currentCountry, countryDataItem))
+                        countryDataItem = []
+                        currentCountry = ''
                 if -1 == self.countryCol or 0 < self.countryFilterList.count(row[self.countryCol]):
-                    try:
-                        date = datetime.strptime(row[self.dateCol], self.dateFormat)
-                    except:
-                        #print("Wrong date Format")
-                        self.dateFormat
-                    else:
-                        if currentCountry == '':
-                            if -1 != self.countryCol:
-                                currentCountry = row[self.countryCol]
-                            else:
-                                currentCountry = self.countryFilterList[0]
-                        if date >= startDate:
-                            countryDataItem.append(row[self.deathCol].split('.')[0])
-                            foundItem = True
-                elif foundItem == True:
-                    foundItem = False
-                    self.countryData.append(GenericCountryDeathData(currentCountry, countryDataItem))
-                    countryDataItem = []
-                    currentCountry = ''
+                    date = datetime.strptime(row[self.dateCol], self.dateFormat)
+                    if currentCountry == '':
+                        if -1 != self.countryCol:
+                            currentCountry = row[self.countryCol]
+                        else:
+                            currentCountry = self.countryFilterList[0]
+                    if date >= startDate:
+                        countryDataItem.append(row[self.deathCol].split('.')[0])
+                        foundItem = True
         if countryDataItem != []:
             self.countryData.append(GenericCountryDeathData(currentCountry, countryDataItem))
 
     def getDeaths(self):
         return self.countryData
 
-#this function will be used for downloading and converting all data. For now it's hard to handle RKI xlsx.
-def parseRkiPercent(val):
-    if 0 != val:
-        return format(val*100, '.2f') + '%'
-    return ''
 
-def parseRkiFloat(val):
-    if 0 != val:
-        return int(val)
-    return ''
-
-def downloadData():
-    #download and prepare polish data
-    path = '../OfficialDataSrc/Poland/DeathData/'
-    filename = 'arch.zip'
-    req = urllib.request.Request(polUrl, headers={'User-Agent' : "Magic Browser"}) 
-    con = urllib.request.urlopen( req )
-    with open(path + filename, 'wb') as file:
-        file.write(con.read())
-    with ZipFile(path + filename, 'r') as zipObj:
-        zipObj.extractall(path)
-    
-    #download and prepare german data
-    path = '../OfficialDataSrc/Germany_RKI/'
-    filename = 'Fallzahlen_Gesamtuebersicht.xlsx'
-    
-    req = urllib.request.Request(deUrl, headers={'User-Agent' : "Magic Browser"}) 
-    con = urllib.request.urlopen( req )
-    with open(path + filename, 'wb') as file:
-        file.write(con.read())
-    xlsx = pd.ExcelFile(path + filename)
-    cols = ['Anzahl COVID-19-Fälle', 'Differenz Vortag Fälle', 'Todesfälle', 'Differenz Vortag Todesfälle', 'Fälle ohne Todesfälle']
-
-    df = xlsx.parse(header=2)
-    for col in cols:
-        df[col] = df[col].fillna(value=0).apply(parseRkiFloat)
-    df['Fall-Verstorbenen-Anteil'] = df['Fall-Verstorbenen-Anteil'].fillna(value=0).apply(parseRkiPercent)
-    df.to_csv(path + filename.split('.')[0] + '.csv', encoding='utf-8', index=False)
-    
-    #download and prepare OWID data
-    path = '../OfficialDataSrc/OWID/'
-    filename = 'owid-covid-data.csv'
-    req = urllib.request.Request(owidUrl, headers={'User-Agent' : "Magic Browser"}) 
-    con = urllib.request.urlopen( req )
-    with open(path + filename, 'wb') as file:
-        file.write(con.read())
-    
-    return True
-
-def cleanupData():
-    os.remove('../OfficialDataSrc/Poland/DeathData/arch.zip')
-    os.remove('../OfficialDataSrc/OWID/owid-covid-data.csv')
-    os.remove('../OfficialDataSrc/Germany_RKI/Fallzahlen_Gesamtuebersicht.xlsx')
-    
 if __name__ == '__main__':
-    
-    if True == downloadData():
-        csvDict = {'Poland' : 'Polska zgony sam COV',
-                   'Germany' : 'Niemcy zgony COV',
-                   'Netherlands' : 'Holandia zgony COV',
-                   'United Kingdom' : 'Wielka Brytania zgony COV'}
-        countryList = []
-        pol = PolishDeathData()
-        owid = GenericDeathData(3, 2, 8, '../OfficialDataSrc/OWID/owid-covid-data.csv',
-                                countryFilterList = ["Netherlands", "United Kingdom"])
-        de = GenericDeathData(0, -1, 4, '../OfficialDataSrc/Germany_RKI/Fallzahlen_Gesamtuebersicht.csv',
-                                '%m/%d/%Y', ['Germany'])
-        pol.parser()
-        pol.createFullCsv()
-        de.parser()
-        owid.parser()
-        countryList.append(pol.getDeathsFromDate(startDate))
-        countryList.extend(de.getDeaths())
-        countryList.extend(owid.getDeaths())
-        
-        date_range = pd.date_range(start=startDate.strftime('%Y-%m-%d'), end=datetime.utcnow(), freq='d')
-        df = pd.DataFrame()
-        df['Data'] = date_range
-        for elem in countryList:
-            df[csvDict[elem.name]] = pd.Series(elem.data)
-        
-        with open("../calculatedData/3months.csv", 'w') as csvFile:
-            csvFile.write(df.to_csv(index = False))
-        cleanupData()
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    logFormat = '%(asctime)s: %(message)s'
+    logLvl = logging.WARNING
+    if config['LOGGING']['EnableInfo'] == 'yes':
+        logLvl = logging.INFO
+    logging.basicConfig(format=logFormat, level=logLvl)
+    logging.info('Running script for ' + config['COUNTRIES']['List'] + ' countries')
+    dataCtx = DataHandler()
+    if True == dataCtx.downloadData():
+        csvCreator = GenericCsvDeathCreator(config['COUNTRIES']['List'].split(','), config['COUNTRIES']['CsvHdr'].split(','))
+        csvCreator.createCsv(datetime.utcnow().replace(hour=0, minute=0, second=0, microsecond=0) - timedelta(int(config['DATA_RANGE']['AllCountries'])))
+        dataCtx.cleanupData()
 
--- Scripts/updateData.sh
+++ Scripts/updateData.sh
@@ -1,32 +1,12 @@
 #!/bin/bash
 
-#get RKI report and convert it to csv
-cd ../OfficialDataSrc/Germany_RKI
-wget https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Fallzahlen_Gesamtuebersicht.xlsx?__blob=publicationFile -O Fallzahlen_Gesamtuebersicht.xlsx
-libreoffice --headless --convert-to csv Fallzahlen_Gesamtuebersicht.xlsx
-#"csv:Text - txt - csv (StarCalc):59,34,76,1,1/1" Fallzahlen_Gesamtuebersicht.xlsx
-rm Fallzahlen_Gesamtuebersicht.xlsx
-cd -
-
-#get OWID data
-cd ../OfficialDataSrc/OWID
-wget https://covid.ourworldindata.org/data/owid-covid-data.csv -O owid-covid-data.csv
-#./owidExtractor.py
-#rm owid-covid-data.csv
-cd -
-
 #get Polish data
 cd ../OfficialDataSrc/Poland
-wget https://arcgis.com/sharing/rest/content/items/a8c562ead9c54e13a135b02e0d875ffb/data -O DeathData/arch.zip
-unzip -oqq DeathData/arch.zip -d DeathData
-rm DeathData/arch.zip
-#./MSZExtractor.py
 wget https://basiw.mz.gov.pl/api/download/file?fileName=covid_pbi/zakaz_zgony_BKO/zgony.csv -O zgony.csv
 wget https://basiw.mz.gov.pl/api/download/file?fileName=covid_pbi/zakaz_zgony_BKO/zakazenia.csv -O zakazenia.csv
 ./deathAnalyser.py
 ./positiveTestAnalyser.py
 cd -
 
+#handle all other data
 ./deathAnalyser.py
-#cleanup
-rm ../OfficialDataSrc/OWID/owid-covid-data.csv
